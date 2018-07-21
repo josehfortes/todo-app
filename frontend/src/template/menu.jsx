d87af5cedba259cf57c6b2c2d9e66611cdc2e1b3
Jose Henrique <vasco0007@gmail.com>
2018-07-21 13:57:12 -0300

    criado menu e configurado routes

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 
 export default props => (
-    <nav className='navbar navbar-inverse bg-inverse'>
-      <div className='container'>
-          <div className='navbar-header'>
-              <a className='navbar-brand' href='#'>
-                <i className='fa fa-calendar-check-o'></i> TodoApp
-              </a>
-          </div>
-          <div id='navbar' className='navbar-collapse collapse'>
-              <ul className="nav navbar-nav">
-                  <li><a href='#/todos'>Tarefas</a></li>
-                  <li><a href='#/about'>Sobre</a></li>
-              </ul>
-          </div>
-      </div>
-      </nav>
+    <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+            <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="#">
+                    <i className='fa fa-calendar-check-o'></i> TodoApp
+                </a>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
+                    <li><a href="#/todos">Tarefas</a></li>
+                    <li><a href="#/about">Sobre</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 )
