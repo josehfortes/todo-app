02a75a66f5fc63985cb1eb3fff753d152041679d
Jose Henrique <vasco0007@gmail.com>
2018-07-21 13:43:39 -0300

    criado componentes menu e paginas

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,8 +3,15 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
 
+import Todo from '../todo/todo'
+import About from '../about/about'
+import Menu from '../template/menu'
+
 export default props => (
-    <div className='container'>
-        <h1>Teste</h1>
+    <div>
+        <Menu/>
+        <Todo/>
+        <About/>
     </div>
+    
 )
