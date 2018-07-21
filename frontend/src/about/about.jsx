e08d6e77b4c64115896dcb35559398121348f4bf
Jose Henrique <vasco0007@gmail.com>
2018-07-21 14:00:43 -0300

    criado componente header para paginas e criado pagina about

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import PageHeader from '../template/pageHeader'
+
+
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós' />
+        <h2>História</h2>
+            <p>Lorem ipsum...</p>
+        <h2>Missão</h2>
+            <p>Lorem ipsum...</p>
+        <h2>Visão</h2>
+            <p>Lorem ipsum...</p>
     </div>
 )
